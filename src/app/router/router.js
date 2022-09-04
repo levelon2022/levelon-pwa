8b3beafb4a562f2898de7ac920656343e7bd32e3
levelon2022 <levelon.in@gmail.com>
2022-09-04 15:07:35 +0530

    entire project setup

--- src/app/router/router.js
+++ src/app/router/router.js
@@ -4,13 +4,13 @@ import { Routes, Route } from "react-router-dom"
 import Home from "../pages/home"
 import Appointments from "../pages/appointments"
 import ReduxTutorial from "../pages/reduxtuti"
-// import Header from "../containers/header"
+import Header from "../components/header"
 
 
 const Router = () => {
 	return (
 		<div>
-			{/* <Header/> */}
+			<Header/>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="appointments" element={<Appointments />} />

13c1a0be5844210798b2f5bba2bd20619f04bfd8
levelon2022 <levelon.in@gmail.com>
2022-09-04 12:55:55 +0530

    Updates

---
+++ precache-manifest.24c99f52be204b9151bebb9311dce61a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "40696b327eab4245e674",
+    "url": "/levelon-pwa/static/css/main.5fa127b2.chunk.css"
+  },
+  {
+    "revision": "40696b327eab4245e674",
+    "url": "/levelon-pwa/static/js/main.40696b32.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/levelon-pwa/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/levelon-pwa/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "6a69f11c2ece1252fa0d",
+    "url": "/levelon-pwa/static/js/runtime~main.6a69f11c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/levelon-pwa/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1148be6dc72d2562dfe34fa79c74d5f0",
+    "url": "/levelon-pwa/index.html"
+  }
+];

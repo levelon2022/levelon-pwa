f4fc51caca861d751146b5a8716d125e5a91b548
levelon2022 <levelon.in@gmail.com>
2022-09-04 14:06:20 +0530

    sass loaders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.scss"
 
-function App() {
-  return (
-    <div className="App">
-     hello json
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="App">
+hello json
+		</div>
+	)
 }
 
-export default App;
+export default App

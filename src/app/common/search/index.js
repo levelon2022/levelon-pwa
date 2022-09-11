27376616277773beaf3d51d0413d226b24e86f1a
levelon2022 <levelon.in@gmail.com>
2022-09-11 10:02:35 +0530

    searchbar

---
+++ src/app/common/search/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+import TextField from "@mui/material/TextField"
+import Box from "@mui/material/Box"
+import IconButton from "@mui/material/IconButton"
+import InputAdornment from "@mui/material/InputAdornment"
+import SearchIcon from "@mui/icons-material/Search"
+import styles from "./search.module.scss"
+
+const SearchBar = () => {
+	return (
+		<>
+			<Box component="form">
+				<TextField
+					underlineShow={false}
+					className={styles.searchBar}
+					placeholder="Search here..."
+					sx={{
+						"& .MuiOutlinedInput-root": {
+							"&.Mui-focused fieldset": {
+								border: "0.5px solid #050303",
+							},
+						},
+					}}
+					variant="outlined"
+					InputProps={{
+						className: styles.searchBar__in,
+
+						endAdornment: (
+							<InputAdornment>
+								<IconButton>
+									<SearchIcon className={styles.searchBar__icon} />
+								</IconButton>
+							</InputAdornment>
+						),
+					}}
+				/>
+			</Box>
+		</>
+	)
+}
+
+export default SearchBar
--- src/app/components/main/index.js
+++ src/app/components/main/index.js
@@ -1,6 +1,10 @@
 import React from "react"
 import BackgroundImg from "../../../assets/images/backgrounds/main.jpg"
 import styles from "./main.module.scss"
+import Grid from "@mui/material/Grid"
+import Box from "@mui/material/Box"
+import { Typography } from "@mui/material"
+import SearchBar from "../../common/search"
 
 const MainSection = () => {
 	return (
@@ -10,6 +14,21 @@ const MainSection = () => {
 				className={styles.main__image}
 				alt="background"
 			/>
+			<Grid className={styles.main__contents}>
+				<Box className={styles.main__contents__primary}> 
+					<Typography  className={styles.main__contents__primary__title}>
+					Choose & Connect With Experts
+					</Typography>
+					<Typography  className={styles.main__contents__primary__description}>
+					Ask Your Queations any time any where Get the best career gudience from the best people in the world.
+					</Typography>
+					<SearchBar/>
+				</Box>
+				<Box className={styles.main__contents__secondary}> 
+				
+				</Box>
+
+			</Grid>
 		</div>
 	)
 }
